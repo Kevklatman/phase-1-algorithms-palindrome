092e31b6f5fa8e4af22280aef8a9ae545d199627
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-28 15:38:16 -0600

    complete

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-function isPalindrome(word) {
-  let inputWord = word.toLowerCase();
-  let reverseInputWord = inputWord.split('').reverse().join('');
-  if (inputWord === reverseInputWord) {
-    return true;
-  } else {
-    return (false);
+function isPalindrome(str) {
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
   }
+  return true;
 }
 
 
 /* 
   Add your pseudocode here
-take string and make all caps or all lowercase so they can be read equally
-split the string into its components an array would Work
-reverse the returned components
-join the components back together so they can be evaluated against og input
+Define a function isPalindrome that takes str as an argument.
+Loop through the string from the start to the middle:
+For each character at position i, compare it with the character at position length - 1 - i.
+If any characters don't match, return false.
+If the loop completes without finding mismatches, return true.
 
 /*
-  Add written explanation of your solution here
-  I separate the word into its components using split and then use reverse which is a function that reverses the order of an array. I then use join to turn the array
-  back into a word so that it can now be evaluated against its mirrored self by setting up an if/ else statement.
+1. The function isPalindrome takes a single argument str, which is the string to check.
+Loop Through the String:
+The loop runs from the start of the string to the middle. (because second half of word is same)
+2. For each character at position i, compare it with the character at position str.length - 1 - i.
+If these characters are different, the string is not a palindrome, and the function returns false.
 */
 
 // You can run `node index.js` to view these console logs
